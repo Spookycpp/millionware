db2af54f6e5b3b9f4f43a34508674a5cd1db69a7
laine <qlaasic@gmail.com>
2020-12-04 19:30:55 +1100

    brain moment

--- millionware/hooks/override_config.cpp
+++ millionware/hooks/override_config.cpp
@@ -8,7 +8,7 @@ bool __fastcall hooks::override_config_hook(uintptr_t ecx, uintptr_t edx, materi
 	config->full_bright = config::get<bool>(FNV_CT("misc.other.world.full_bright"));
 	config->draw_gray= config::get<bool>(FNV_CT("misc.other.world.draw_grey"));
 	config->show_low_res_image = config::get<bool>(FNV_CT("misc.other.world.low_resolution"));
-	config->show_mip_levels = config::get<bool>(FNV_CT("misc.other.world.mip_levels"));
+	config->show_mip_levels = config::get<bool>(FNV_CT("misc.other.world.rainbow"));
 
 	return reinterpret_cast<decltype(&override_config_hook)>(hooks::override_config.original)(ecx, edx, config, update);
 }
