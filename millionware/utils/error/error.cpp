0ee94104b815649d3a0d3bf435fd332d4b8542ad
laine <qlaasic@gmail.com>
2021-02-18 12:42:59 +1100

    fixing error message (kind of)

--- millionware/utils/error/error.cpp
+++ millionware/utils/error/error.cpp
@@ -8,9 +8,9 @@ void utils::report_error(std::string_view error_message) {
 	const auto report_id = 0xDEADBEEF;
 	// @todo: report error to the server
 
-	char buffer[128];
+	char buffer[1024];
 
-	sprintf_s(buffer, sizeof buffer, STR_ENC("Please reach out to us via ticket providing the report ID: %08X"), report_id);
+	sprintf_s(buffer, sizeof buffer, STR_ENC("Please reach out to us via ticket providing the report ID: %08X\n%s"), report_id, error_message.data());
 
 	MessageBoxA(nullptr, buffer, nullptr, MB_OK | MB_ICONERROR);
 
