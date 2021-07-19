c4d0938a07c022e1627b3030ec2fd387195bf839
laine <qlaasic@gmail.com>
2021-07-19 12:36:14 +1000

    fsn cleanup

--- sdk/hooks/frame_stage_notify.cpp
+++ sdk/hooks/frame_stage_notify.cpp
@@ -14,16 +14,19 @@ void __fastcall hooks::frame_stage_notify(c_base_client_dll *ecx, uintptr_t edx,
 
     discord_rpc::update();
 
+    // main menu blur can be removed
+    features::miscellaneous::panorama_blur();
+
+    //features::miscellaneous::server_selector();
+
     if (!interfaces::engine_client->is_in_game() || !interfaces::engine_client->is_connected())
         return frame_stage_notify_original(ecx, edx, frame_stage);
 
-    if (frame_stage == e_client_frame_stage::FRAME_STAGE_START) {
+    if (frame_stage == e_client_frame_stage::FRAME_STAGE_START)
         cheat::local_player = (c_player *) interfaces::entity_list->get_entity(interfaces::engine_client->get_local_player());
-    }
 
-    if (frame_stage == e_client_frame_stage::FRAME_STAGE_RENDER_START) {
+    if (frame_stage == e_client_frame_stage::FRAME_STAGE_RENDER_START)
         features::visuals::weather::update_weather();
-    }
 
     features::miscellaneous::on_frame_stage_notify(frame_stage);
     features::lag_compensation::on_frame_stage_notify(frame_stage);
@@ -32,5 +35,4 @@ void __fastcall hooks::frame_stage_notify(c_base_client_dll *ecx, uintptr_t edx,
     util::on_frame_stage_notify(frame_stage);
 
     frame_stage_notify_original(ecx, edx, frame_stage);
-
 }
