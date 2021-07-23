9166bb55715d721038c4e31f37ad8c5f091723ec
laine <qlaasic@gmail.com>
2021-07-24 07:29:44 +1000

    forgot config boolean on hook

--- sdk/hooks/get_demo_playback_parameters.cpp
+++ sdk/hooks/get_demo_playback_parameters.cpp
@@ -2,13 +2,14 @@
 
 #include "../core/hooks/hooks.h"
 #include "../core/interfaces/interfaces.h"
+#include "../core/settings/settings.h"
 #include "../engine/hash/hash.h"
 
 c_demo_playback_paramaters *__fastcall hooks::get_demo_playback_parameters(uintptr_t ecx, uintptr_t edx) {
 
     const auto parameters = get_demo_playback_parameters_original(ecx, edx);
 
-    if (parameters /* config boolean here*/ && (uintptr_t)_ReturnAddress() != patterns::get_demo_file_end_reached()) {
+    if (parameters && settings.miscellaneous.reveal_overwatch_suspect && (uintptr_t)_ReturnAddress() != patterns::get_demo_file_end_reached()) {
         static c_demo_playback_paramaters custom_parameters;
         custom_parameters = *parameters;
         custom_parameters.anonymous_player_identity = false;
