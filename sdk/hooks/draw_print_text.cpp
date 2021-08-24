1a8e784a00a4df9cf006ebf30fdc505767578c7b
eternity <jeramie@protonmail.com>
2021-08-24 01:40:30 -0700

    Accent color on net_graph text.

--- sdk/hooks/draw_print_text.cpp
+++ sdk/hooks/draw_print_text.cpp
@@ -1,16 +1,19 @@
-
-#include <stdio.h>
-#include <string.h>
+#include <string>
 #include "../core/hooks/hooks.h"
 #include "../core/interfaces/interfaces.h"
-
 #include "../engine/security/xorstr.h"
+#include "../ui/ui.h"
 
 bool __fastcall hooks::draw_print_text(uintptr_t ecx, uintptr_t edx, const wchar_t *text, int text_length, int draw_type) {
-
     if (wcsncmp(text, xs(L"fps"), 3) == 0) {
-        std::wstring millionware_string = std::wstring(xs(L"[millionware] ")) + text;
-        return draw_print_text_original(ecx, edx, millionware_string.data(), millionware_string.length(), draw_type);
+        color_t color = ui::get_accent_color();
+        interfaces::surface->set_text_color(color.r, color.g, color.b, color.a);
+
+        auto millionware_string = std::wstring(xs(L"[millionware] "));
+        draw_print_text_original(ecx, edx, millionware_string.data(), millionware_string.length(), draw_type);
+
+        interfaces::surface->set_text_color(229, 229, 178, 255); // reset to default color
+        return draw_print_text_original(ecx, edx, text, text_length, draw_type);
     }
 
     return draw_print_text_original(ecx, edx, text, text_length, draw_type);
