90f13edc2c01fe9fcb81b5ada565989c641236c9
eternity <jeramie@protonmail.com>
2021-08-26 16:19:30 -0700

    Port `vec2d`, `vec3d`.

--- sdk/lua/types/lua_vec2d.hpp
+++ sdk/lua/types/lua_vec2d.hpp
@@ -2,114 +2,117 @@
 
 #include "../../source engine/vector.h"
 
-class vec2d {
-public:
-	float x{}, y{};
+namespace lua_internal::types {
+    class vec2d {
+    public:
+        float x, y;
 
-	vec2d() : x{ 0.0f }, y{ 0.0f } {}
-	vec2d(const float x, const float y) : x{ x }, y{ y } {}
+        vec2d() : x{ 0.0f }, y{ 0.0f } {}
+        vec2d(const float x, const float y) : x{ x }, y{ y } {}
 
-    vec2d(const point_t &point) {
-        *this = point;
-    }
+        vec2d &operator=(point_t &point) {
+            x = point.x;
+            y = point.y;
+            return *this;
+        }
 
-    vec2d &operator=(point_t &point) {
-        x = point.x;
-        y = point.y;
-        return *this;
-    }
+        vec2d operator+(const vec2d &v) const {
+            return vec2d(x + v.x, y + v.y);
+        }
 
-    vec2d operator+(const vec2d& v) const {
-        return vec2d(x + v.x, y + v.y);
-    }
+        vec2d operator-(const vec2d &v) const {
+            return vec2d(x - v.x, y - v.y);
+        }
 
-    vec2d operator-(const vec2d& v) const {
-        return vec2d(x - v.x, y - v.y);
-    }
+        vec2d mul(const vec2d &vec) const {
+            return vec2d(x * vec.x, y * vec.y);
+        }
 
-    vec2d mul(const vec2d& vec) const {
-        return vec2d(x * vec.x, y * vec.y);
-    }
+        vec2d operator*(const float &fl) const {
+            return vec2d(x * fl, y * fl);
+        }
 
-    vec2d operator*(const float& fl) const {
-        return vec2d(x * fl, y * fl);
-    }
-	
-    vec2d operator/(const vec2d& vec) const {
-        return vec2d(x / vec.x, y / vec.y);
-    }
+        vec2d operator/(const vec2d &vec) const {
+            return vec2d(x / vec.x, y / vec.y);
+        }
 
-    vec2d& operator/=(const float& fl) {
-        const float a = 1.0f / fl;
+        vec2d &operator/=(const float &fl) {
+            const float a = 1.0f / fl;
 
-        x *= a;
-        y *= a;
+            x *= a;
+            y *= a;
 
-        return *this;
-    }
+            return *this;
+        }
 
-    float length() const {
-        return sqrt(x * x + y * y);
-    }
+        float length() const {
+            return sqrt(x * x + y * y);
+        }
 
-    float length_sqr() const {
-        return x * x + y * y;
-    }
+        float length_sqr() const {
+            return x * x + y * y;
+        }
 
-    float dot(const vec2d& vec) const {
-        return x * vec.x + y * vec.y;
-    }
+        float dot(const vec2d &vec) const {
+            return x * vec.x + y * vec.y;
+        }
 
-    float dist(const vec2d& vec) const {
-        vec2d delta;
+        float dist(const vec2d &vec) const {
+            vec2d delta;
 
-        delta.x = x - vec.x;
-        delta.y = y - vec.y;
+            delta.x = x - vec.x;
+            delta.y = y - vec.y;
 
-        return delta.length();
-    }
+            return delta.length();
+        }
 
-    void normalize() {
-        *this /= length();
-    }
+        void normalize() {
+            *this /= length();
+        }
 
-    vec2d lerp(const vec2d& to, const float t) {
-        return to * t + *this * (1.0f - t);
-    }
+        vec2d lerp(const vec2d &to, const float t) {
+            return to * t + *this * (1.0f - t);
+        }
 
-	bool empty() {
-        return x == 0.0f && y == 0.0f;
-	}
+        bool empty() {
+            return x == 0.0f && y == 0.0f;
+        }
 
-	bool valid() {
-        return std::isfinite(x) && std::isfinite(y);
-	}
+        bool valid() {
+            return std::isfinite(x) && std::isfinite(y);
+        }
 
-	void zero() {
-        this->x = 0;
-        this->y = 0;
-	}
-};
+        void zero() {
+            this->x = 0;
+            this->y = 0;
+        }
+    };
+}
 
-inline void lua_vec2d(lua_State* l) {
-    /*luabridge::getGlobalNamespace(l)
-    .beginClass<vec2d>(xs("vec2d"))
-        .addConstructor<void(*)(const float&, const float&)>()
-        .addData(xs("x"), &vec2d::x)
-        .addData(xs("y"), &vec2d::y)
-        .addFunction(xs("__add"), &vec2d::operator+)
-        .addFunction(xs("__sub"), &vec2d::operator-)
-        .addFunction(xs("__mul"), &vec2d::mul)
-        .addFunction(xs("__div"), &vec2d::operator/)
-        .addFunction(xs("__len"), &vec2d::length)
-        .addFunction(xs("length"), &vec2d::length)
-        .addFunction(xs("length_sqr"), &vec2d::length_sqr)
-        .addFunction(xs("dot"), &vec2d::dot)
-        .addFunction(xs("dist"), &vec2d::dist)
-        .addFunction(xs("normalize"), &vec2d::normalize)
-        .addFunction(xs("lerp"), &vec2d::lerp)
-        .addFunction(xs("empty"), &vec2d::empty)
-        .addFunction(xs("valid"), &vec2d::valid)
-        .addFunction(xs("zero"), &vec2d::zero)
-	.endClass();*/
+inline void lua_internal::context::lua_vec2d() const {
+    auto lua = sol::state_view(l);
+
+    auto vec2d_table = lua.new_usertype<types::vec2d>("vec2d",
+        sol::call_constructor, sol::factories([](const float &x, const float &y) {
+            return std::make_shared<types::vec2d>(x, y);
+        })
+    );
+
+    vec2d_table["x"] = &types::vec2d::x;
+    vec2d_table["y"] = &types::vec2d::y;
+
+    vec2d_table["__add"] = &types::vec2d::operator+;
+    vec2d_table["__sub"] = &types::vec2d::operator-;
+    vec2d_table["__mul"] = &types::vec2d::mul;
+    vec2d_table["__div"] = &types::vec2d::operator/;
+    vec2d_table["__len"] = &types::vec2d::length;
+
+    vec2d_table["length"] = &types::vec2d::length;
+    vec2d_table["length_sqr"] = &types::vec2d::length_sqr;
+    vec2d_table["dot"] = &types::vec2d::dot;
+    vec2d_table["normalize"] = &types::vec2d::normalize;
+    vec2d_table["lerp"] = &types::vec2d::lerp;
+    vec2d_table["empty"] = &types::vec2d::empty;
+    vec2d_table["valid"] = &types::vec2d::valid;
+    vec2d_table["zero"] = &types::vec2d::zero;
 }
--- sdk/lua/types/lua_vec3d.hpp
+++ sdk/lua/types/lua_vec3d.hpp
@@ -1,117 +1,125 @@
 #pragma once
 
-class vec3d {
-  public:
-    float x, y, z;
+namespace lua_internal::types {
+    class vec3d {
+    public:
+        float x, y, z;
 
-    vec3d() : x{0.0f}, y{0.0f}, z{0.0f} {}
-    vec3d(const float x, const float y, const float z) : x{x}, y{y}, z{z} {}
+        vec3d() : x{ 0.0f }, y{ 0.0f }, z{ 0.0f } {}
+        vec3d(const float x, const float y, const float z) : x{ x }, y{ y }, z{ z } {}
 
-    vec3d operator+(const vec3d &v) const {
-        return vec3d(x + v.x, y + v.y, z + v.z);
-    }
+        vec3d operator+(const vec3d &v) const {
+            return vec3d(x + v.x, y + v.y, z + v.z);
+        }
 
-    vec3d operator-(const vec3d &v) const {
-        return vec3d(x - v.x, y - v.y, z - v.z);
-    }
+        vec3d operator-(const vec3d &v) const {
+            return vec3d(x - v.x, y - v.y, z - v.z);
+        }
 
-    vec3d mul(const vec3d &v) const {
-        return vec3d(x * v.x, y * v.y, z * v.z);
-    }
+        vec3d mul(const vec3d &v) const {
+            return vec3d(x * v.x, y * v.y, z * v.z);
+        }
 
-    vec3d operator*(const float &fl) const {
-        return vec3d(x * fl, y * fl, z * fl);
-    }
+        vec3d operator*(const float &fl) const {
+            return vec3d(x * fl, y * fl, z * fl);
+        }
 
-    vec3d operator/(const vec3d &v) const {
-        return vec3d(x / v.x, y / v.y, z / v.z);
-    }
+        vec3d operator/(const vec3d &v) const {
+            return vec3d(x / v.x, y / v.y, z / v.z);
+        }
 
-    vec3d &operator/=(const float &fl) {
-        const float a = 1.0f / fl;
+        vec3d &operator/=(const float &fl) {
+            const float a = 1.0f / fl;
 
-        x *= a;
-        y *= a;
-        z *= a;
+            x *= a;
+            y *= a;
+            z *= a;
 
-        return *this;
-    }
+            return *this;
+        }
 
-    float length() const {
-        return std::sqrt(x * x + y * y + z * z);
-    }
+        float length() const {
+            return std::sqrt(x * x + y * y + z * z);
+        }
 
-    float length_2d() const {
-        return std::sqrtf(x * x + y * y);
-    }
+        float length_2d() const {
+            return std::sqrtf(x * x + y * y);
+        }
 
-    float length_sqr() const {
-        return x * x + y * y + z * z;
-    }
+        float length_sqr() const {
+            return x * x + y * y + z * z;
+        }
 
-    float length_2d_sqr() const {
-        return x * x + y * y;
-    }
+        float length_2d_sqr() const {
+            return x * x + y * y;
+        }
 
-    float dot(const vec3d &v) const {
-        return x * v.x + y * v.y + z * v.z;
-    }
+        float dot(const vec3d &v) const {
+            return x * v.x + y * v.y + z * v.z;
+        }
 
-    float dist(const vec3d &v) const {
-        vec3d delta;
+        float dist(const vec3d &v) const {
+            vec3d delta;
 
-        delta.x = x - v.x;
-        delta.y = y - v.y;
-        delta.z = z - v.z;
+            delta.x = x - v.x;
+            delta.y = y - v.y;
+            delta.z = z - v.z;
 
-        return delta.length();
-    }
+            return delta.length();
+        }
 
-    void normalize() {
-        *this /= length();
-    }
+        void normalize() {
+            *this /= length();
+        }
 
-    vec3d lerp(const vec3d &to, const float t) {
-        return to * t + *this * (1.0f - t);
-    }
+        vec3d lerp(const vec3d &to, const float t) {
+            return to * t + *this * (1.0f - t);
+        }
 
-    bool empty() {
-        return x == 0.0f && y == 0.0f && z == 0.0f;
-    }
+        bool empty() {
+            return x == 0.0f && y == 0.0f && z == 0.0f;
+        }
 
-    bool valid() {
-        return std::isfinite(x) && std::isfinite(y) && std::isfinite(z);
-    }
+        bool valid() {
+            return std::isfinite(x) && std::isfinite(y) && std::isfinite(z);
+        }
 
-    void zero() {
-        this->x = 0;
-        this->y = 0;
-        this->z = 0;
-    }
-};
+        void zero() {
+            this->x = 0;
+            this->y = 0;
+            this->z = 0;
+        }
+    };
+}
 
-inline void lua_vec3d(lua_State *l) {
-    /*luabridge::getGlobalNamespace(l)
-        .beginClass<vec3d>(xs("vec3d"))
-        .addConstructor<void (*)(const float &, const float &, const float &)>()
-        .addData(xs("x"), &vec3d::x)
-        .addData(xs("y"), &vec3d::y)
-        .addData(xs("z"), &vec3d::z)
-        .addFunction(xs("__add"), &vec3d::operator+)
-        .addFunction(xs("__sub"), &vec3d::operator-)
-        .addFunction(xs("__mul"), &vec3d::mul)
-        .addFunction(xs("__div"), &vec3d::operator/)
-        .addFunction(xs("__len"), &vec3d::length)
-        .addFunction(xs("length"), &vec3d::length)
-        .addFunction(xs("length_sqr"), &vec3d::length_sqr)
-        .addFunction(xs("length_2d"), &vec3d::length_2d)
-        .addFunction(xs("length_2d_sqr"), &vec3d::length_2d_sqr)
-        .addFunction(xs("dot"), &vec3d::dot)
-        .addFunction(xs("dist"), &vec3d::dist)
-        .addFunction(xs("normalize"), &vec3d::normalize)
-        .addFunction(xs("lerp"), &vec3d::lerp)
-        .addFunction(xs("empty"), &vec3d::empty)
-        .addFunction(xs("valid"), &vec3d::valid)
-        .addFunction(xs("zero"), &vec3d::zero)
-        .endClass();*/
+inline void lua_internal::context::lua_vec3d() const {
+    auto lua = sol::state_view(l);
+
+    auto vec3d_table = lua.new_usertype<types::vec3d>("vec3d",
+        sol::call_constructor, sol::factories([](const float &x, const float &y, const float &z) {
+            return std::make_shared<types::vec3d>(x, y, z);
+        })
+    );
+
+    vec3d_table["x"] = &types::vec3d::x;
+    vec3d_table["y"] = &types::vec3d::y;
+    vec3d_table["z"] = &types::vec3d::z;
+    
+    vec3d_table["__add"] = &types::vec3d::operator+;
+    vec3d_table["__sub"] = &types::vec3d::operator-;
+    vec3d_table["__mul"] = &types::vec3d::mul;
+    vec3d_table["__div"] = &types::vec3d::operator/;
+    vec3d_table["__len"] = &types::vec3d::length;
+    
+    vec3d_table["length"] = &types::vec3d::length;
+    vec3d_table["length_sqr"] = &types::vec3d::length_sqr;
+    vec3d_table["length_2d"] = &types::vec3d::length_2d;
+    vec3d_table["length_2d_sqr"] = &types::vec3d::length_2d_sqr;
+    vec3d_table["dot"] = &types::vec3d::dot;
+    vec3d_table["dist"] = &types::vec3d::dist;
+    vec3d_table["normalize"] = &types::vec3d::normalize;
+    vec3d_table["lerp"] = &types::vec3d::lerp;
+    vec3d_table["empty"] = &types::vec3d::empty;
+    vec3d_table["valid"] = &types::vec3d::valid;
+    vec3d_table["zero"] = &types::vec3d::zero;
 }
