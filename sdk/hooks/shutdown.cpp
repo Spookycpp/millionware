08f7ec8f5a58fe1a0f08cc5539faa01d8842e156
laine <qlaasic@gmail.com>
2021-12-25 05:22:07 +1100

    forgot to finish comment lmfao

--- sdk/hooks/shutdown.cpp
+++ sdk/hooks/shutdown.cpp
@@ -2,15 +2,14 @@
 #include "../core/hooks/hooks.h"
 
 void __fastcall hooks::shutdown(uintptr_t ecx, uintptr_t edx) {
-    // purpose:
-    // prevent unhandledExceptionFilter from
-    // printing shit relating back to the
-    // event listener when you close the game
-    // can be used to prevent process 
+    // moneybot.cc/onetap.com (v1).
 
-    // inform other thread that we've closed the game
+    // prevent unhandledExceptionFilter from printing shit relating back to the event listener when you close the game.
+    // also can be used to prevent process from hanging, crashing or in some cases dumping.
+
+    // inform other thread that we've closed the game.
     cheat::panic = true;
 
-    // call shutdown, resulting in the process closing, without our module present
+    // call shutdown, resulting in the process closing, without our module present.
     shutdown_original(ecx, edx);
 }
